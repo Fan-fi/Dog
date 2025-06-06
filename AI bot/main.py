a98fecb2524fef3458367bdf7db978916b577c85
Fan-fi <naboychenko1201@gmail.com>
2025-06-06 14:15:03 +0300

    Update main.py

--- AI bot/main.py
+++ AI bot/main.py
@@ -2,7 +2,7 @@ import telebot
 from bot_logic import gen_pass, gen_emodji, flip_coin, opredelenie_dog  # Импортируем функции из bot_logic
 
 # Замени 'TOKEN' на токен твоего бота
-bot = telebot.TeleBot("7801153685:AAGtb6z0OMsEmWDn0pzSAThVltqFrABEiuY")
+bot = telebot.TeleBot("")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
